6a4a206ca77964049e12286e2a7cc04e4932b7b4
Dayasagar Salian <38876768+dayasagarsalianr@users.noreply.github.com>
2025-09-13 21:04:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Cost to cut a board into squares/minimum-cost-to-cut-a-board-into-squares.cpp
@@ -0,0 +1,50 @@
+class Solution {
+  public:
+    int minCost(int n, int m, vector<int>& x, vector<int>& y) {
+        //  code here
+        sort(x.rbegin(),x.rend());
+        sort(y.rbegin(),y.rend());
+        
+        int i=0;
+        int j=0;
+        
+      
+        
+        int h =1;
+        int v =1;
+        int ans =0;
+        
+        while(i<m-1 && j<n-1){
+            if(x[i]>y[j]){
+                //Cut vertically
+                //Vertical pices increases by 1
+                
+                ans+=(x[i]*h);
+                v++;
+                i++;
+            }else{
+                
+                //Cut horizontally
+                //Horizontally pices increases by 1
+                
+                ans+=(y[j]*v);
+                h++;
+                j++;
+            }
+        }
+        
+        while(i<m-1){
+             ans+=(x[i]*h);
+                v++;
+                i++;
+        }
+        
+        while(j<n-1){
+             ans+=(y[j]*v);
+                h++;
+                j++;
+        }
+        
+        return ans;
+    }
+};
